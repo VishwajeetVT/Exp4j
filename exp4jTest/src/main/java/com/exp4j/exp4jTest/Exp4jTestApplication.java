7bffba516e148715c6cc9ec9ea50862b92220c9e
Vishwajeet Kotkar <109328444+VishwajeetVT@users.noreply.github.com>
2022-12-05 11:22:23 +0530

    Add files via upload
    First Commit

---
+++ exp4jTest/src/main/java/com/exp4j/exp4jTest/Exp4jTestApplication.java
@@ -0,0 +1,13 @@
+package com.exp4j.exp4jTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exp4jTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exp4jTestApplication.class, args);
+	}
+
+}
---
+++ exp4jTest/src/test/java/com/exp4j/exp4jTest/Exp4jTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp4j.exp4jTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exp4jTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
